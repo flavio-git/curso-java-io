1b95a55994cdccf703ab2b96680f03f5f6bc2592
flavio-git <flaviovicentini1991@gmail.com>
2022-09-22 01:33:14 -0300

    aula 3.1 finalizada

---
+++ src/com/flavio/javaio/test/TesteEscrita2.java
@@ -0,0 +1,19 @@
+package com.flavio.javaio.test;
+
+import java.io.*;
+
+public class TesteEscrita2 {
+    public static void main(String[] args) throws IOException {
+
+        // Escrevendo arquivos com a classe FileWriter
+
+        FileWriter file = new FileWriter("teste-escrita2.txt");
+
+        file.write("This is a test.");
+        file.write(System.lineSeparator()); // o mesmo resultado de '\n' só que ele ver qual o sistema operacional
+        file.write("Class 3.1");
+
+        file.close();
+
+    }
+}
---
+++ src/com/flavio/javaio/test/TesteEscrita3.java
@@ -0,0 +1,21 @@
+package com.flavio.javaio.test;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TesteEscrita3 {
+    public static void main(String[] args) throws IOException {
+
+        // Escrevendo arquivos com a classe BufferedWriter e FileWriter
+
+        BufferedWriter file = new BufferedWriter(new  FileWriter("teste-escrita3.txt"));
+
+        file.write("This is a test with BufferedWriter and FileWriter.");
+        file.newLine();
+        file.write("Class 3.1");
+
+        file.close();
+
+    }
+}
