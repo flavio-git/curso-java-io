cac3dc046a9d76a2026f03c1cd26a9f3faff67b9
flavio-git <flaviovicentini1991@gmail.com>
2022-09-22 01:53:36 -0300

    aula 3.2 finalizada

---
+++ src/com/flavio/javaio/test/TesteEscrita4.java
@@ -0,0 +1,27 @@
+package com.flavio.javaio.test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+
+public class TesteEscrita4 {
+    public static void main(String[] args) throws IOException {
+
+        // Escrevendo arquivos com as classes PrintStream e PrintWriter
+        // Antes do Java ter as classes FileWriter era utilizada a classe PrintStream
+
+        PrintStream file = new PrintStream("teste-escrita4.txt");
+        PrintWriter file2 = new PrintWriter("teste-escrita5.txt");
+
+        file.println("This a test with PrintStream class.");
+        file.print("Class 3.2.");
+
+        file2.println("This a test with PrintWriter class.");
+        file2.print("Class 3.2.");
+
+        file.close();
+        file2.close();
+
+    }
+}
